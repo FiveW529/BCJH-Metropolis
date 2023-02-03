fb51cbccc71a398b155859585c3819fa75b13180
FiveW529 <123658474+FiveW529@users.noreply.github.com>
2023-02-03 07:42:30 -0800

    Update toolEquipped.hpp

--- toolEquipped.hpp
+++ toolEquipped.hpp
@@ -30,18 +30,18 @@ void toolEquipped(Chef *chef) {
 
     if (chef->name == "无限师父") {             // 金锄头
         skill.ability.bake += 150;              // 全技法+30
-        skill.abilityBuff.cut += 20; // 蔬菜售价+15%
+        skill.abilityBuff.knife += 20; // 蔬菜售价+15%
         chef->tool = NO_TOOL;               // 不允许装备其他厨具
     }
-    if (chef->name == "宋书汐汐") {   // 幸福礼花炮
-        skill.ability.stirfry += 10; // 炒技法+150
-        skill.abilityBuff.bake += 2; // 烤售价+25%
-        chef->tool = NO_TOOL;         // 不允许装备其他厨具
-    }
-    if (chef->name == "柔可") {
-        skill.abilityBuff.knife += 2;
-        skill.materialBuff.creation += 1;
-        chef->tool = NO_TOOL;
-    }
+//     if (chef->name == "宋书汐汐") {   // 幸福礼花炮
+//         skill.ability.stirfry += 10; // 炒技法+150
+//         skill.abilityBuff.bake += 2; // 烤售价+25%
+//         chef->tool = NO_TOOL;         // 不允许装备其他厨具
+//     }
+//     if (chef->name == "柔可") {
+//         skill.abilityBuff.knife += 2;
+//         skill.materialBuff.creation += 1;
+//         chef->tool = NO_TOOL;
+//     }
 }
 #endif
